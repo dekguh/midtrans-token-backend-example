5fea7bc1259dac342d34edfc50ff9b17e5083030
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-17 18:23:56 +0800

    mpb

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('M.P.B')
+    res.send('Mars perut bunyi')
 })
 
 app.post('/generate-trx-token', (req, res) => {
