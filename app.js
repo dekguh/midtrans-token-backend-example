b25f60efa720b9ac3b0da0caa681db79972e30ac
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-17 17:37:29 +0800

    corsss

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors');
 
-app.use(cors({origin: [
-    'http://localhost:3000',
-    'https://mentoro-rental-motorcycle-front-end.vercel.app/'
-]}));
-app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('M.P.B')
