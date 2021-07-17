824a7fefabdac111c0608d07119254dd86e290f4
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-17 15:18:26 +0800

    add cors

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors');
 
-app.use(cors({
-    origin: [
-        'https://mentoro-rental-motorcycle-front-end.vercel.app',
-        'http://localhost:3000'
-    ]
-}));
+app.use(cors());
 
 app.use(express.json())
 
