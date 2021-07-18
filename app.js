05e6b2aa0b559e469983774b538583a039a22282
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-18 17:40:25 +0800

    cors

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const midtransClient = require('midtrans-client')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 const cors = require('cors')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -24,11 +20,11 @@ app.post('/generate-trx-token', (req, res) => {
 
     const parameterSnap = {
         transaction_details: {
-            order_id: req.body.orderId,
-            gross_amount: req.body.grossAmount
+            "order_id": req.body.orderId,
+            "gross_amount": req.body.grossAmount
         }
     }
-    snap.createTransaction(JSON.stringify(parameterSnap))
+    snap.createTransaction(parameterSnap)
     .then((transaction)=>{
         res.json(transaction)
     })
